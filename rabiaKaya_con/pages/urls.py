2d349929a41e727f2e96537a4381f5c464746aae
Rabia Kaya <98751296+rabiakaya0@users.noreply.github.com>
2024-07-14 23:28:54 +0300

    ana taslak hazır

--- rabiaKaya_con/pages/urls.py
+++ rabiaKaya_con/pages/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from pages.views import IndexView
+from pages.views import IndexView, WhoView, ElectricView, ElectronicView, SoftwareUnityView, SoftwareDjangoView, ContactView
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
+    path('/kimdir', WhoView.as_view(), name="kimdir"),
+    path('/elektrik', ElectricView.as_view(), name="elektrik"),
+    path('/elektronik', ElectronicView.as_view(), name="elektronik"),
+    path('/yazilim/unity', SoftwareUnityView.as_view(), name="yazilimUnity"),
+    path('/yazilim/django', SoftwareDjangoView.as_view(), name="yazilimDjango"),
+    path('/iletisim', ContactView.as_view(), name="iletisim"),
 ]
--- rabiaKaya_con/pages/views.py
+++ rabiaKaya_con/pages/views.py
@@ -5,3 +5,21 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+class WhoView(TemplateView):
+    template_name = 'kimdir.html'
+
+class ElectricView(TemplateView):
+    template_name = 'elektrik.html'
+
+class ElectronicView(TemplateView):
+    template_name = 'elektronik.html'
+
+class SoftwareUnityView(TemplateView):
+    template_name = 'yazilimUnity.html'
+
+class SoftwareDjangoView(TemplateView):
+    template_name = 'yazilimDjango.html'
+
+class ContactView(TemplateView):
+    template_name = 'iletisim.html'
